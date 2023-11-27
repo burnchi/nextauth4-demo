193ad82379c2374360ebd76bbf505a2a18add4e7
burnchi <1273732363@qq.com>
2023-11-28 00:56:26 +0800

    can use signin API

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,33 @@
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                }
+            },
+            async authorize(credentials) {
+                // This is where you need to retrieve user data 
+                // to verify with credentials
+                // Docs: https://next-auth.js.org/configuration/providers/credentials
+                const user = { id: "42", name: "admin", password: "admin" }
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+}
---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+
+export const user = [
+    {
+        id:1,
+        name:"xiaoming",
+        password:"123"
+    },
+    {
+        id:2,
+        name:"xiaohong",
+        password:"456"
+    }
+]
